6ec9d016b76825515af58c56132c6819c63677e2
dk <matison@gmail.com>
2025-05-20 02:29:26 +0300

    fix: export GroupMember in db models __init__ for correct imports

--- src/db/models/__init__.py
+++ src/db/models/__init__.py
@@ -5,6 +5,11 @@ from src.db.models.chat_message import ChatMessage
 from src.db.models.blocked_user import BlockedUser
 from src.db.models.chat import Chat
 from src.db.models.user_state import UserState
+from src.db.models.question import Question
+from src.db.models.group import Group
+from src.db.models.answer import Answer
+from src.db.models.group_member import GroupMember
+from .group_member import GroupMember
 
 __all__ = [
     "User",
@@ -14,4 +19,8 @@ __all__ = [
     "BlockedUser",
     "Chat",
     "UserState",
+    "Question",
+    "Group",
+    "Answer",
+    "GroupMember",
 ] 
